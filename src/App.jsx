7ef796aa782d5d388799a37f455feaf685fed061
kartik0920 <kartik0920@gmail.com>
2025-02-28 00:15:43 +0530

    Just created navbar in signup page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./assets/components/navbar";
+
+function App() {
+  return (
+    <>
+      {/* use for nav bar in siginPage */}
+      <Navbar />
+      <h2>hello</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,11 @@
+// Navbar componet used and AIODYSSEY title
+
+export default function Navbar() {
+  return (
+    <>
+      <section className="navbar-login">
+        <span>AI ODYSSEY</span>
+      </section>
+    </>
+  );
+}
