8b5cbd578e0c4b9708faec24a5d63fdc3e5d5f41
prajakta <prajaktamondhe3@gmail.com>
2025-02-28 10:28:15 +0530

    Created Sign In component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./assets/components/navbar";
-
+import SignIn from "./assets/components/SignIn";
+import './App.css'
+import AIorNot from "./assets/components/AIorNot";
 function App() {
   return (
     <>
       {/* use for nav bar in siginPage */}
-      <Navbar />
-      <h2>hello</h2>
+      <Navbar /> {/* fixed */}
+      <AIorNot/> {/* fixed */}
+      <SignIn/>  {/* changing */}
     </>
   );
 }
