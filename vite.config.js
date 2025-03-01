9b0cd51340f0ca2372ef2c4ae9784105647217aa
kartik0920 <kartik0920@gmail.com>
2025-03-01 17:14:49 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
+      external: ["react-router-dom"], // Explicitly externalize it
     },
   },
 });
