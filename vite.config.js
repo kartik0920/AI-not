a58e35a62beec7ff03fa66cbeb9482ab6e6a686d
kartik0920 <kartik0920@gmail.com>
2025-03-01 17:20:26 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom"], // Explicitly externalize it
+      external: ["react-router-dom", "@supabase/supabase-js"], // Explicitly externalize it
     },
   },
 });
